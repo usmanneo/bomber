cd3afd676614a137862b37a17d6b589d111401f0
usmanneo <h4k3rtools@gmail.com>
2024-07-10 23:57:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/send-otp', methods=['POST'])
 def send_otp():
